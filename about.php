77a4255d9a34b4482d0d633541f3c19814cf5722
Yazan Al-hemyari <yazanhameed456@gmail.com>
2022-06-19 16:13:11 +0300

    about us page

---
+++ about.php
@@ -0,0 +1,45 @@
+<?php
+include "header.php";
+?>
+  </div>
+  <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="detail-box">
+            <h6>
+              About
+            </h6>
+            <div class="custom_heading-container">
+              <h2>
+                web elements
+              </h2>
+              <hr>
+            </div>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when
+              looking at its layout. The point of using Lorem
+            </p>
+            <div>
+              <a href="">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/about-img.png" alt="">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- end about section -->
+
+
+  <?php
+include "footer.php";
+?>
