57b7a168e47e47784f7ed9a8ea12b314a8467062
Yazan Al-hemyari <yazanhameed456@gmail.com>
2022-06-19 16:17:38 +0300

    Portfolio page

---
+++ portfolio.php
@@ -0,0 +1,81 @@
+<?php
+include "header.php";
+?>
+  </div>
+
+
+  <!-- portfolio section -->
+
+  <section class="portfolio_section layout_padding">
+    <div class="container">
+      <div class="custom_heading-container">
+        <h2>
+          Portfolio
+        </h2>
+        <hr>
+      </div>
+      <p>
+        There are many variations of passages of Lorem Ipsum available, but the majority
+      </p>
+      <div class="layout_padding2-top">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-1.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-2.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-3.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-4.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-5.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img src="images/p-6.jpg" alt="">
+              <a href="">
+                <img src="images/link.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end portfolio section -->
+
+
+
+  <?php
+include "footer.php";
+?>
