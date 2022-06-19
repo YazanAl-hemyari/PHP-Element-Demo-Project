a98172e84abc6205cdad94a72a3492e090148ac1
Yazan Al-hemyari <yazanhameed456@gmail.com>
2022-06-19 16:15:14 +0300

    Footer for all pages

---
+++ footer.php
@@ -0,0 +1,52 @@
+ <!-- info section -->
+ <section class="info_section layout_padding">
+    <div class="container">
+      <div class="info_social">
+        <div>
+          <a href="">
+            <img src="images/fb.png" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="images/twitter.png" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="images/linkedin.png" alt="">
+          </a>
+        </div>
+        <div>
+          <a href="">
+            <img src="images/insta.png" alt="">
+          </a>
+        </div>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+
+  <!-- footer section -->
+  <section class="container-fluid footer_section">
+    <p>
+      &copy; 2019 All Rights Reserved By
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </section>
+  <!-- footer section -->
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+</body>
+
+</html>
